f19af5cc816c2e96967540dae8e7e75a27fe2aa8
Codespider2 <wachiraamos402@gmail.com>
2024-05-30 13:54:40 +0300

    responsive pages

--- src/components/provider/HowItWorks.tsx
+++ src/components/provider/HowItWorks.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 const HowItWorks = () => {
   return (
     <div className="pb-3">
-      <div className="w-full sm:h-[400px] flex justify-center items-center max-md:flex-col max-md:h-max  max-md:w-screen">
-        <div className="w-[1000px] sm:h-[300px]  md-max:space-y-4 flex justify-between max-md:flex-col max-md:h-max max-md:w-max">
-          <div className=" sm:shadow-lg gap-4 h-full max-md:h-max   flex flex-col justify-center items-center">
-            <div className="w-[182px]   flex justify-center items-center  max-md:w-screen ">
+      <div className="w-full sm:h-[400px] flex justify-center items-center max-md:flex-col max-md:h-screen max-md:w-screen">
+        <div className="w-full sm:w-[1000px] sm:h-[300px] flex flex-col sm:flex-row justify-between max-md:space-y-4 sm:space-y-0 max-md:h-full max-md:w-screen">
+          <div className="sm:shadow-lg gap-4 h-full max-md:h-max flex flex-col justify-center items-center">
+            <div className="w-[182px] flex justify-center items-center max-md:w-full">
               <Image
                 src="/visibility.svg"
                 alt="visibility"
@@ -15,13 +15,15 @@ const HowItWorks = () => {
                 height={150}
               />
             </div>
-            <div className=" font-bold text-[24px]">Increased Visibilty</div>
-            <div className="w-[308px] h-[38px] text-center text-[16px]  text-wrap">
+            <div className="font-bold text-[24px] text-center">
+              Increased Visibility
+            </div>
+            <div className="w-[308px] h-[38px] text-center text-[16px] text-wrap">
               Reach more patients by showcasing your services
             </div>
           </div>
-          <div className=" gap-4 h-full sm:shadow-lg  flex flex-col justify-center items-center">
-            <div className="w-[182px]   flex justify-center items-center ">
+          <div className="gap-4 h-full sm:shadow-lg flex flex-col justify-center items-center">
+            <div className="w-[182px] flex justify-center items-center">
               <Image
                 src="/patients.svg"
                 alt="patients"
@@ -29,17 +31,16 @@ const HowItWorks = () => {
                 height={150}
               />
             </div>
-            <div className=" font-bold text-[24px]">
+            <div className="font-bold text-[24px] text-center">
               Enhanced Patient Engagement
             </div>
-
             <div className="w-[308px] h-[38px] text-center text-[16px] text-wrap">
-              Improve patient satisfaction with easy scheduling option,
-              communications, and timely reminders.{" "}
+              Improve patient satisfaction with easy scheduling options,
+              communications, and timely reminders.
             </div>
           </div>
-          <div className=" gap-4 h-full sm:shadow-lg  flex flex-col justify-center items-center">
-            <div className="w-[182px]   flex justify-center items-center ">
+          <div className="gap-4 pt-2 h-full sm:shadow-lg flex flex-col justify-center items-center">
+            <div className="w-[182px] flex justify-center items-center">
               <Image
                 src="/efficiency.svg"
                 alt="efficiency"
@@ -47,9 +48,10 @@ const HowItWorks = () => {
                 height={150}
               />
             </div>
-            <div className=" font-bold text-[24px]">Efficient Scheduling</div>
-
-            <div className="w-[308px] text-[16px] h-[38px] text-center  text-wrap">
+            <div className="font-bold text-[24px] text-center">
+              Efficient Scheduling
+            </div>
+            <div className="w-[308px] h-[38px] text-center text-[16px] text-wrap">
               Manage your appointments seamlessly with our intuitive platform
             </div>
           </div>
